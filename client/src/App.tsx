2dfee3fd8f9d862226acce6e401c3a96d7dd53f5
wayenae <ynaddegrano@iskolarngbayan.pup.edu.ph>
2025-08-28 22:31:14 +0800

    scroll to top function/update files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
+
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,6 +14,7 @@ import Community from "./pages/Community";
 import PreAssessment from "./pages/PreAssessment";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,6 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
