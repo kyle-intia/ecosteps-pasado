0729a29185a0ef84cc12ee15a5b85858f54389eb
kyle-intia <kwestaguirre@gmail.com>
2025-10-27 00:20:41 +0800

    Default changes

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ const seedAchievements = require('./utils/seedAchievement');
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: ['https://ecosteps-online.netlify.app'],
+    origin: ['https://ecosteps-online.netlify.app', 'http://localhost:8080', 'http://localhost:3000'],
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
--- server/utils/cookies.js
+++ server/utils/cookies.js
@@ -5,11 +5,13 @@ const date_1 = require("./date");
 const env_1 = require("../constants/env");
 exports.REFRESH_PATH = "/auth/refresh";
 const secure = env_1.NODE_ENV !== "development";
-const domain = env_1.NODE_ENV === "development" ? "localhost" : "ecostep.online";
+const sameSite = env_1.NODE_ENV === "development" ? "lax" : "none";
+const domain = env_1.NODE_ENV === "development" ? undefined : "ecosteps-online.netlify.app";
 const defaults = {
-    sameSite: "none",
+    sameSite,
     httpOnly: true,
     secure,
+    ...(domain && { domain }),
 };
 const getAccessTokenCookieOptions = () => ({
     ...defaults,
