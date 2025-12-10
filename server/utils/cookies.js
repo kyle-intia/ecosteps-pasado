5bddde3d8d224cc75594d8f316a0eeba5e10016b
paaaaaaads <neilpatrickmpadron@iskolarngbayan.pup.edu.ph>
2025-12-10 19:31:02 +0800

    cookies v02

--- server/utils/cookies.js
+++ server/utils/cookies.js
@@ -5,15 +5,11 @@ const date_1 = require("./date");
 const env_1 = require("../constants/env");
 exports.REFRESH_PATH = "/auth/refresh";
 const secure = env_1.NODE_ENV !== "development";
-const domain = env_1.NODE_ENV === "development"
-  ? undefined              
-  : "ecosteps-pasado-production.up.railway.app";
 
 const defaults = {
   sameSite: "none",
   httpOnly: true,
   secure,
-  ...(domain && { domain })  
 };
 const getAccessTokenCookieOptions = () => ({
     ...defaults,
